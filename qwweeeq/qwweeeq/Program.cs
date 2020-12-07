45b43c5664245a2686e205053f18900565aa127d
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:02:46 +0200

    Add files via upload

---
+++ qwweeeq/qwweeeq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace qwweeeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            int maxq = Math.Max(x, y);
+            int maximum = Math.Max(maxq, z);
+            int minq = Math.Min(x, y);
+            int minimum = Math.Min(minq, z);
+            Console.WriteLine(maximum);
+            Console.WriteLine(minimum);
+        }
+    }
+}
