1673a501628fb742fe0b6c65dfa6d4b98cb1b343
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:05:27 +0200

    Add files via upload

---
+++ 40/40/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToInt32(Console.ReadLine());
+            if (x <= y)
+                x = 0;
+            Console.WriteLine(x);
+         
+        }
+    }
+}
