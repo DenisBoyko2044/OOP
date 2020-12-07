0b86a5d009d611685643779d1ee155568cc12a61
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:05:55 +0200

    Add files via upload

---
+++ 42/42/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (x != y)
+            {
+                if (x < y)
+                {
+                    x = (x + y) / 2;
+                    y = 2 * x * y;
+                }
+                else if (x > y)
+                {
+                    y = (x + y) / 2;
+                    x = 2 * x * y;
+                }
+            }
+           
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
