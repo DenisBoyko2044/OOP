4b97c3b8ca9de091704ba901035306b00231dd95
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:03:34 +0200

    Add files via upload

---
+++ 36/36/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                double a = Convert.ToInt32(Console.ReadLine());
+                double b = Convert.ToInt32(Console.ReadLine());
+                double c = Convert.ToInt32(Console.ReadLine());
+            if (a < b || b < c)
+                Console.WriteLine("да");
+            else if (a > b || b > c)
+                Console.WriteLine("ne");
+            else if (a == b || b == c)
+                Console.WriteLine("так незя");
+        }
+    }
+}
