7a9f5bc0d810a9f143bdca6db2484b8b9a102f15
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:04:30 +0200

    Add files via upload

---
+++ 38a/38a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _38a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z = 0;
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+                z = x - y;
+            else if (x < y)
+                z = y - x + 1;
+            Console.WriteLine(z);
+        }
+    }
+}
