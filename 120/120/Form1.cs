4a15c5160303c2bf21d85baf6076b57b20ae9f36
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:26:04 +0200

    Add files via upload

---
+++ 120/120/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _120
+{
+
+    public partial class Form1 : Form
+    {    int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (i == 2)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                g.Clear(Color.White);
+                SolidBrush clBlack = new SolidBrush(Color.Black);
+                Pen myWind = new Pen(Color.Yellow, 2);
+                g.FillPolygon(clBlack, new Point[]
+        {
+             new Point(120,100),new Point(140,120),
+             new Point(140,120),new Point(140,140),
+             new Point(140,140),new Point(120,160),
+             new Point(120,160),new Point(100,140),
+             new Point(100,140),new Point(100,120),
+             new Point(100,120),new Point(120,100)
+
+        });
+                i = 2;
+            }
+            if (i == 1)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                g.Clear(Color.White);
+                SolidBrush clBlack = new SolidBrush(Color.Black);
+                Pen myWind = new Pen(Color.Yellow, 2);
+                g.FillPolygon(clBlack, new Point[]
+        {
+             new Point(80,80),new Point(170,80),
+             new Point(170,80),new Point(170,150),
+             new Point(170,150),new Point(80,150),
+             new Point(80,150),new Point(80,80),
+
+        });
+                i = 2;
+            }
+            if (i == 0)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                g.Clear(Color.White);
+                SolidBrush clBlack = new SolidBrush(Color.Black);
+                Pen myWind = new Pen(Color.Yellow, 2);
+                g.FillPolygon(clBlack, new Point[]
+        {
+             new Point(100,100),new Point(150,100),
+             new Point(150,100),new Point(80,170),
+             new Point(80,170),new Point(100,100)
+            
+        }) ;
+                i = 1;
+            }
+           
+
+
+            }
+        }
+    }
+
---
+++ 80b/80b/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _80b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            int fact = 1;
+            double s = x;
+            int n = 13;
+            for (int i = 1; i <n; i+=2)
+            {
+                fact = fact * i;
+                s = (s - (Math.Pow(x, i)) / fact) * (-1);
+              
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
