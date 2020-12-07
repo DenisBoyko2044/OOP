1765c3f80e3eb39bee3d7a9a41f7b7a83a5a91da
DenisBoyko2044 <71661048+DenisBoyko2044@users.noreply.github.com>
2020-12-07 17:34:34 +0200

    Add files via upload

---
+++ lr2/lr2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lr2
+{
+    public class Abonent
+    {
+        public string Pib;
+        public int number;
+        public int teleph;
+        public string info;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Клиент: {Pib}  Номер: {number} Телефон{teleph}  Инфо{info}");
+        }
+    }
+    class Prog {
+        static void Main(string[] args)
+        {
+            Abonent vcx = new Abonent();
+            vcx.Pib = Console.ReadLine();
+            vcx.number = Convert.ToInt32(Console.ReadLine());
+            vcx.teleph = Convert.ToInt32(Console.ReadLine());
+            vcx.info = Console.ReadLine();
+            vcx.GetInfo();
+        }
+    
+}
+}
